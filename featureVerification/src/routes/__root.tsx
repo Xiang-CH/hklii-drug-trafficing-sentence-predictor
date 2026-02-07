4e3903e83c60c2b10cf8fdcaf197af476c405b27
Xiang <cxiang@connect.hku.hk>
2026-02-07 23:20:15 +0800

    fix: git case ignore error

--- featureVerification/src/routes/__root.tsx
+++ featureVerification/src/routes/__root.tsx
@@ -12,7 +12,7 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
-import Header from '@/components/Header'
+import Header from '@/components/header'
 
 interface MyRouterContext {
   queryClient: QueryClient
